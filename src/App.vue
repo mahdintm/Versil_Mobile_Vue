a0ea4d50be00a2ed6dd4510c85431e5ada431eeb
Toofan <Toofan534@gmail.com>
2022-08-18 18:30:28 +0430

    All files has been uploaded

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  mounted() {
+    if ("alt" in window) {
+      alt.emit("ClientWEB:PhoneWebView:Ready");
+    }
+  }
+}
+</script>
+
+<style>
+</style>
